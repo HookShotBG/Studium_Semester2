cf5ef672af7d8d96651f8f5b312f052e3774a41a
daniellutziger <daniel.lutziger@hispeed.ch>
2019-02-18 10:48:10 +0100

    aufg 1

---
+++ src/sw1/Rueckgeld.java
@@ -0,0 +1,24 @@
+package sw1;
+
+import java.util.Scanner;
+
+public class Rueckgeld {
+
+    public static void main(String[] args) {
+        int preis = 105; // Preis des Produkts in Rappen
+        int bezahlt = 500; // Eingeworfener Betrag in Rappen
+        // Dein Code
+
+        //Ausgabe
+    }
+
+    public int erhaltenerBetrag(){
+        Scanner betrag = new Scanner(System.in);
+        betrag.
+        return
+    }
+
+    public void print() {
+
+    }
+}
---
+++ src/sw1/Ueberholweg.java
@@ -0,0 +1,28 @@
+package sw1;
+
+public class Ueberholweg {
+
+    public static void main(String[] args) {
+
+        double fzA = 5; // Länge von deinem Auto (Meter)
+        double fzB = 15; // Länge des anderen Autos (Meter)
+        double vA = 80; // Deine Geschwindigkeit (km/h)
+        double vB = 70; // Geschwindigkeit des anderen Autos(km/h)
+        double s1 = vA/2;
+        double s2 = vB/2;
+        double relativStrecke = fzA + s1 + fzB + s2;
+        double geschwindigkeitRelativ = vA - vB;
+        double zeit = relativStrecke / geschwindigkeitRelativ * 3.6;
+        double ueberholweg = zeit * vA / 3.6;
+
+        System.out.println("Geschwindigkeit A: "+vA); System.out.println("Geschwindigkeit B: "+vB); System.out.println("Länge A: "+fzA); System.out.println("Länge B: "+fzB);
+        System.out.println("Sicherheitsabstand 1: " + s1);
+        System.out.println("Sicherheitsabstand 2: " + s2);
+        System.out.println("Relative Strecke: " + relativStrecke);
+        System.out.println("Relative Geschwindigkeit: " + geschwindigkeitRelativ + " km/h");
+        System.out.println("Benötigte Zeit: " + zeit + " Sekunden");
+        System.out.println("Überholweg: " + ueberholweg    + " Meter");
+
+    }
+
+}
